21e76c8a79c60e3c3d67dc1ea0fd75b87c0012ef
godfreydoo <godfrey.doo@gmail.com>
2021-06-30 03:07:52 -0700

    Finish app to run tests sequentially and extract session results into a text file

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # retrieve-loader-test-data
+
+This repo helps you perform your load testing on [Loader.io](https://loader.io/) by
+1. sequentially executing prepared tests with the same duration,
+2. extracting test results from current run, and
+3. writing to a text file in CSV format for further analysis.
+
+
+## Limitations
+- assumes all tests have the same duration
+- includes a buffer time between running the next test (you could change the core functions to check for the status before running subsequent tests)
+- test names must be unique in the `testSuite` object
+
+## Installation
+1. Fork and clone this repository.
+2. Install dependencies
+```bash
+$ npm install
+```
+3. Create a `.env` file with your Loader.io API key. An example file has been provided.
+4. Update `index.js` file with your test configurations.
+- `duration` is the duration of your test cases.
+- `buffer` is a buffer time between when the next test case runs. This is important, as Loader.io does not allow for more than one running test at once.
+- `testSuite` is an object with the key as the name of your test, and value as your test id. Test names must be unique.
+5. Run to start tests
+```
+node index.js
+```
+6. A text file will be created in CSV format once finished.
+
+Test id can be found in your individual test, under "Webhooks & Schedule section". More information about Loader.io's API documentation can be found [here](https://docs.loader.io/).
---
+++ index.js
@@ -0,0 +1,118 @@
+const converter = require('json-2-csv');
+const axios = require('axios');
+const fs = require('fs');
+require('dotenv').config();
+
+
+
+// Define your test ids, test names, and test parameters
+var duration = 30000; // in ms, ie. 30 seconds
+var buffer = 5000; // in ms, i.e. 5 second buffer (I found this to be a good buffer)
+var url = 'https://api.loader.io/v2/tests'
+var testSuite = {
+  '<your test nam123123e>': '<your test id>',
+  '<your test name1231>': '<your test id>',
+  '<your test name12312>': '<your test id>'
+};
+
+
+
+
+
+
+
+
+
+
+
+// Core functions -- do not change anything below!
+function LoaderTests() {
+  this._testResults = [];
+  this._testSuite = [];
+}
+
+LoaderTests.prototype.runTest = function (testName, testidx){
+  console.log(`${testName} test is running...`);
+  return new Promise((resolve, reject) => {
+    const config = {
+      method: 'put',
+      url: `${url}/${testidx}/run`,
+      headers: {
+        'loaderio-auth': process.env.LOADER_API_KEY
+      }
+    };
+    axios(config)
+      .then(response => {
+        const obj =
+        this._testSuite.push({
+          testName: testName,
+          testidx: testidx,
+          resultidx: response.data.result_id
+        });
+        resolve();
+      })
+      .catch(error => {
+        console.log(`Something went wrong with running testidx: ${testidx}`);
+        console.log(error);
+        reject();
+      })
+  })
+}
+
+LoaderTests.prototype.getTestResults = async function (testName, testidx, resultidx){
+  var testName = testName;
+  var resultidx = resultidx;
+  const config = {
+    url: `${url}/${testidx}/results/${resultidx}`,
+    headers: {
+      'loaderio-auth': process.env.LOADER_API_KEY
+    }
+  }
+
+  await axios(config)
+    .then(response => {
+      response.data['test_name'] = testName;
+      this.write2CSV();
+      this._testResults.push(response.data);
+    })
+    .catch(error => {
+      console.log(`Something went wrong with fetching test results for resultidx: ${resultidx}`);
+      console.log(error);
+    });
+}
+
+LoaderTests.prototype.write2CSV = async function () {
+  const writeCSVStream = fs.createWriteStream('results.txt');
+  await converter.json2csv(this._testResults, (err, csv) => {
+    if (err) {
+      console.log('Writing from json2csv has an error: ', err);
+      return;
+    }
+    writeCSVStream.write(csv);
+  })
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, duration + buffer));
+}
+
+async function run() {
+  var newRun = new LoaderTests;
+  try {
+    console.log('Starting to run tests...');
+    for (var key in testSuite) {
+      await sleep(newRun.runTest(key, testSuite[key]))
+    }
+    console.log('All tests ran...');
+    console.log('Fetching data and writing to CSV file...');
+    await newRun._testSuite.forEach(value => {
+       newRun.getTestResults(value.testName, value.testidx, value.resultidx)
+    });
+    console.log('results.txt file should be created / updated soon');
+  } catch (err) {
+    console.log('Double check testSuite has right data, and that the duration + buffer is accurate for your test settings');
+    console.log(err);
+  }
+}
+
+run();
